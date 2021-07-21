0a90738ccc7971f9af93d58c074ac1644f66f5ac
Johnnie Pittman <jpittman@group6.net>
2021-07-21 12:10:22 -0700

    Make use of the wrapped UIKit class.

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -7,14 +7,18 @@
 
 import SwiftUI
 
-struct PageView: View {
+struct PageView<Page: View>: View {
+    
+    var pages: [Page]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PageViewController(pages: pages)
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView()
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0)})
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
