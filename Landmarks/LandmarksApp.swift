6510ead42d31b282f3eb74636f04f169f74a4ec1
Johnnie Pittman <jpittman@group6.net>
2021-08-05 23:48:20 -0700

    Handle notifications when running on the watch.

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -17,5 +17,11 @@ struct LandmarksApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+    
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
+    
+
